07f4e5fa3861abe359e9d78e5944dfcc9c3f79fd
citrus <81078642+IWantMyLemons@users.noreply.github.com>
2025-08-21 01:13:37 +0700

    replaced home page since i think it makes more sense

---
+++ src/pages/reader.rs
@@ -0,0 +1,33 @@
+use leptos::prelude::*;
+
+use crate::components::file_browser::FileBrowser;
+
+/// Default Page
+#[component]
+pub fn Reader() -> impl IntoView {
+    view! {
+        <ErrorBoundary fallback=|errors| {
+            view! {
+                <h1>"Uh oh! Something went wrong!"</h1>
+
+                <p>"Errors: "</p>
+                // Render a list of errors as strings - good for development purposes
+                <ul>
+                    {move || {
+                        errors
+                            .get()
+                            .into_iter()
+                            .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                            .collect_view()
+                    }}
+
+                </ul>
+            }
+        }>
+
+            <div class="container">
+                <FileBrowser />
+            </div>
+        </ErrorBoundary>
+    }
+}
