8517185acfec28a28906c1c1cde712d24f1f8633
citrus <81078642+IWantMyLemons@users.noreply.github.com>
2025-08-21 17:53:38 +0700

    refactored and added a remove function for RecentFiles

--- src/state/recent_files.rs
+++ src/state/recent_files.rs
@@ -2,11 +2,13 @@ use leptos::prelude::*;
 use leptos::serde_json;
 
 #[derive(Clone, Debug)]
+/// Manages and syncs recent filenames with LocalStorage
 pub struct RecentFiles {
     pub files: RwSignal<Vec<String>>,
 }
 
 impl RecentFiles {
+    /// Loads files from LocalStorage
     pub fn new() -> Self {
         let files = load_from_storage();
         Self {
@@ -14,24 +16,37 @@ impl RecentFiles {
         }
     }
 
+    /// Adds the filename to recent files and LocalStorage
     pub fn add(&self, file: String) {
-        let mut files = self.files.get();
-        files.push(file);
-        self.files.set(files.clone());
-        save_to_storage(&files);
+        self.files.update(move |files| {
+            files.push(file);
+            save_to_storage(files);
+        });
+    }
+
+    /// Removes a filename by it's index
+    pub fn remove(&self, index: usize) {
+        self.files.update(move |files| {
+            files.remove(index);
+            save_to_storage(files);
+        });
     }
 }
 
+/// Loads "recent_files" from LocalStorage
 fn load_from_storage() -> Vec<String> {
-    let storage = window().local_storage().unwrap().unwrap();
-    let recent_str = storage
-        .get_item("recent_files")
-        .unwrap()
-        .unwrap_or("[]".to_string());
-    serde_json::from_str::<Vec<String>>(&recent_str).unwrap_or_default()
+    window()
+        .local_storage()
+        .ok()
+        .flatten()
+        .and_then(|storage| storage.get_item("recent_files").ok())
+        .flatten()
+        .and_then(|json| serde_json::from_str::<Vec<String>>(&json).ok())
+        .unwrap_or_default()
 }
 
-fn save_to_storage(files: &Vec<String>) {
+/// Saves `files` to "recent_files" in LocalStorage
+fn save_to_storage(files: &[String]) {
     let recent_str = serde_json::to_string(files).unwrap_or("[]".to_string());
     let storage = window().local_storage().unwrap().unwrap();
     storage.set_item("recent_files", &recent_str).unwrap();
